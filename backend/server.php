cf3cea93ef7aa6192a9f8ead019f4d02ea8a2e8a
Camila Rocio Serafini <camiiserafinii@gmail.com>
2025-05-20 16:40:42 -0300

    entendimiento puntos 3 y 4 guia 6

--- backend/server.php
+++ backend/server.php
@@ -22,6 +22,48 @@ if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     exit();
 }
 
-//Inluimos el archivo que define las rutas o la lógica que responderá la petición.
-require_once("./routes/studentsRoutes.php");
+$uri = parse_url($_SERVER['REQUEST_URI']);
+//toma toda la URL y la separa en partes
+//parse_url la convierte en estudiante como:
+/*[
+  'path' => '/routes.php',
+  'query' => 'module=estudiantes'
+]*/
+
+$query = $uri['query'];
+//extrae solo la parte del query string
+//$query = 'module=estudiantes';
+
+parse_str($query,$query_array);
+//$query = 'module=estudiantes';
+//$query_array = ['module' => 'estudiantes'];
+
+$module = $query_array['module'];
+//$query_array = ['module' => 'estudiantes'];
+
+if (!$module) 
+{
+    http_response_code(400);
+    echo json_encode(["error" => "Módulo no especificado"]);
+    exit();
+}
+
+$routeFile = __DIR__ . "/routes/{$module}Routes.php";
+//__DIR__ es una constante de PHP que me devuelve la ruta absoluta del
+//directorio actual, en este caso server.php
+//convierte {$module} teniendo asi la ruta completa del archivo
+
+
+if (file_exists($routeFile)) 
+{
+    require_once($routeFile);
+} 
+else 
+{
+    http_response_code(404);
+    echo json_encode(["error" => "Ruta para el módulo '{$module}' no encontrada"]);
+}
+
+/*Inluimos el archivo que define las rutas o la lógica que responderá la petición.
+require_once("./routes/studentsRoutes.php");*/
 ?>
